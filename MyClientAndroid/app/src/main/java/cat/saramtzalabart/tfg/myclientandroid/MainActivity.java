0c01624b262f647d329fed3acea744127137ebf0
Sara Martínez Alabart <smartineza@edu.tecnocampus.cat>
2020-05-23 12:23:01 +0200

    two method

--- MyClientAndroid/app/src/main/java/cat/saramtzalabart/tfg/myclientandroid/MainActivity.java
+++ MyClientAndroid/app/src/main/java/cat/saramtzalabart/tfg/myclientandroid/MainActivity.java
@@ -3,13 +3,28 @@ package cat.saramtzalabart.tfg.myclientandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
+import org.hl7.fhir.dstu3.model.CodeableConcept;
+import org.hl7.fhir.dstu3.model.Coding;
+import org.hl7.fhir.dstu3.model.DateType;
+import org.hl7.fhir.dstu3.model.Enumerations;
+import org.hl7.fhir.dstu3.model.HumanName;
+import org.hl7.fhir.dstu3.model.IdType;
 import org.hl7.fhir.dstu3.model.Patient;
 import org.hl7.fhir.instance.model.api.IBaseBundle;
 
+import java.util.Date;
+
 import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.rest.api.MethodOutcome;
 import ca.uhn.fhir.rest.client.api.IGenericClient;
+
+import static org.hl7.fhir.dstu3.model.Enumerations.AdministrativeGender.FEMALE;
+import static org.hl7.fhir.dstu3.model.Enumerations.AdministrativeGender.MALE;
+import static org.hl7.fhir.dstu3.model.Enumerations.AdministrativeGender.OTHER;
+import static org.hl7.fhir.dstu3.model.Enumerations.AdministrativeGender.UNKNOWN;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,11 +42,49 @@ public class MainActivity extends AppCompatActivity {
 
         IGenericClient client = ctx.newRestfulGenericClient(serverBase);
 
-        IBaseBundle bundle = client.search().forResource(Patient.class)
+        //textViewResult.setText(getPatient(client, "32222222S").toString());
+        textViewResult.setText("Got ID: " + createPatient(client, "Sara", "Martínez Alabart", "female", "1998-10-11", "32222222S").getValue());
+    }
+
+    private IBaseBundle getPatient(IGenericClient client, String dni){
+        IBaseBundle response = client.search()
+                .forResource(Patient.class)
+                .where(Patient.IDENTIFIER.exactly().code(dni))
+                .returnBundle(IBaseBundle.class)
+                .execute();
+        return response;
+    }
+
+    private IdType createPatient(IGenericClient client, String name, String surname, String gender, String birthDate, String dni){
+        //String resourceType = "Patient";
+        String code = "DNI";
+        String text = "Document Nacional d'Identitat";
+        Enumerations.AdministrativeGender ag= UNKNOWN;
+        if (gender.equals("female")){ ag =  FEMALE; }
+        else if (gender.equals("male")){ ag = MALE; }
+        else { ag = OTHER; }
+
+        Coding coding = new Coding().setCode(code);
+
+        CodeableConcept codeableConcept = new CodeableConcept()
+                .addCoding(coding)
+                .setText(text);
+
+        Patient patient = new Patient();
+        patient.addIdentifier().setType(codeableConcept);
+        patient.addIdentifier().setValue(dni);
+        patient.addName().setFamily(surname).addGiven(name);
+        patient.setGender(ag);
+        //TODO Comprovar que la versió d'api que estic utilitzant sigui la correcta
+        patient.setBirthDate(new Date(birthDate));
+
+        MethodOutcome outcome = client.create()
+                .resource(patient)
                 .prettyPrint()
                 .encodedJson()
                 .execute();
 
-        textViewResult.append(bundle.toString());
+        IdType patientId = (IdType) outcome.getId();
+        return patientId;
     }
 }
