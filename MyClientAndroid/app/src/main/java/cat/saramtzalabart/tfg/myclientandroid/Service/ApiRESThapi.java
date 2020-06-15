4e26a9f248b235d4763ea3acb18af94c64155a60
Sara Martínez Alabart <smartineza@edu.tecnocampus.cat>
2020-06-15 13:01:33 +0200

    thread

--- MyClientAndroid/app/src/main/java/cat/saramtzalabart/tfg/myclientandroid/Service/ApiRESThapi.java
+++ MyClientAndroid/app/src/main/java/cat/saramtzalabart/tfg/myclientandroid/Service/ApiRESThapi.java
@@ -37,9 +37,13 @@ import static org.hl7.fhir.dstu3.model.Enumerations.AdministrativeGender.UNKNOWN
 public class ApiRESThapi {
 
     private UserProvider user;
+    private boolean resultat;
+    private IdType idPatient;
 
     public ApiRESThapi() {
         user = new UserProvider();
+        resultat = false;
+        idPatient = new IdType();
         //this.client = MyContextFhir.getCtx().newRestfulGenericClient(Constants.serverBase);
     }
 
@@ -48,6 +52,14 @@ public class ApiRESThapi {
     }*/
     //SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
 
+    private void setResultat (boolean value){
+        user.setResourceCreated(value);
+        resultat = value;
+    }
+    private void setIdPatient(IdType value){
+        user.setPatientId(value);
+        idPatient = value;
+    }
     /*
     *
     *
@@ -78,18 +90,19 @@ public class ApiRESThapi {
                 .addCoding(coding)
                 .setText(text);
 
-        final Patient patient = new Patient();
+        Patient patient = new Patient();
         patient.addIdentifier().setType(codeableConcept);
         patient.addIdentifier().setValue(dni);
         patient.addName().setFamily(surname).addGiven(name);
         patient.setGender(ag);
         patient.setBirthDate(bd);
 
-        final MethodOutcome[] outcome = new MethodOutcome[1];
+        MethodOutcome[] outcome = new MethodOutcome[1];
         Thread validateThread = new Thread() {
             @Override
             public void run() {
-
+                boolean r = false;
+                IdType id = new IdType();
                 if (update) {
                     patient.setId("Patient/" + user.getPatientId());
                     outcome[0] = MyContextFhir.getClient().update()
@@ -97,32 +110,49 @@ public class ApiRESThapi {
                             .prettyPrint()
                             .encodedJson()
                             .execute();
-                    Log.println(Log.INFO, "S11", "UPDATED: " + String.valueOf(outcome[0].getCreated()));
-                    user.setResourceCreated(outcome[0].getCreated());
-                    IdType patientId = (IdType) outcome[0].getId();
-                    user.setPatientId(patientId);
-                    Log.println(Log.INFO, "S11", "Patient ID updated: " + String.valueOf(user.getPatientId()));
-                } else {
 
+                    //user.setResourceCreated(outcome[0].getCreated());
+                    r = outcome[0].getCreated();
+                    setResultat(r);
+                    Log.println(Log.INFO, "S11", "UPDATED: " + String.valueOf(resultat));
+                    //IdType patientId = (IdType) outcome[0].getId();
+                    id = (IdType) outcome[0].getId();
+                    setIdPatient(id);
+                    //user.setPatientId(patientId);
+                    Log.println(Log.INFO, "S11", "Patient ID updated: " + String.valueOf(idPatient));
+                } else {
                     outcome[0] = MyContextFhir.getClient().create()
                             .resource(patient)
                             .prettyPrint()
                             .encodedJson()
                             .execute();
-                    user.setResourceCreated(outcome[0].getCreated());
-                    Log.println(Log.INFO, "S11", "CREATED: " + String.valueOf(outcome[0].getCreated()));
-                    IdType patientId = (IdType) outcome[0].getId();
-                    user.setPatientId(patientId);
-                    Log.println(Log.INFO, "S11", "Patient ID: " + String.valueOf(user.getPatientId()));
-
+                    //user.setResourceCreated(outcome[0].getCreated());
+                    r = outcome[0].getCreated();
+                    setResultat(r);
+                    Log.println(Log.INFO, "S11", "CREATED: " + String.valueOf(resultat));
+                    //IdType patientId = (IdType) outcome[0].getId();
+                    id = (IdType) outcome[0].getId();
+                    setIdPatient(id);
+                    //user.setPatientId(patientId);
+                    Log.println(Log.INFO, "S11", "Patient ID: " + String.valueOf(idPatient));
                 }
             }
         };
+        //user.setPatientId(idPatient);
+        Log.println(Log.INFO, "S11", "Patient ID user: " + String.valueOf(user.getPatientId()));
+        Log.println(Log.INFO, "S11", "Patient created out of thread: " + String.valueOf(resultat));
         validateThread.start();
+        return resultat;
+
         //return  outcome[0].getCreated();
         /*if (outcome[0].getResource().getIdElement() != null) {return true;}
         else{return false;}*/
-        return user.getResourceCreated();
+        //return user.getResourceCreated();
+        //TODO Comprovar que tots els idPatient de api és posen a USER
+       /* user.setPatientId(idPatient);
+        Log.println(Log.INFO, "S11", "Patient ID user: " + String.valueOf(user.getPatientId()));
+        Log.println(Log.INFO, "S11", "Patient created out of thread: " + String.valueOf(resultat));
+        return resultat;*/
     }
     public boolean createDiagnosticReport (String typeDiabetes){
 
